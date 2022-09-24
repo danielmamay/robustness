eaf28f2026cda5bd6f0728e7d08f647bbe5f0eac
Daniel May <daniel.may.20@ucl.ac.uk>
2022-09-24 18:17:00 +0100

    Add documentation

--- robustness/attacks/fast_gradient.py
+++ robustness/attacks/fast_gradient.py
@@ -1,23 +1,51 @@
+"""Fast Gradient Sign Method (FGSM) adversarial attack.
+
+Fast Gradient Sign Method from 'Explaining and Harnessing Adversarial Examples'
+[https://arxiv.org/abs/1412.6572]
+
+    Typical usage example:
+
+    attack = FastGradient(classifier, epsilon=0.2)
+    adversarial_inputs, predicted_labels = attack.generate(inputs, labels)
+"""
+from typing import Optional, Union
+
+import torch
+from torch import Tensor
 import torch.nn.functional as F
 
 class FastGradient:
-    """
-    Fast Gradient Sign Method (FGSM) from 'Explaining and Harnessing Adversarial Examples'
-    [https://arxiv.org/abs/1412.6572]
-    """
-    def __init__(self, model, epsilon=0.007, clamp_values=(0, 1)):
-        self.model = model
+    """Fast Gradient Sign Method adversarial attack with L_inf distance measure."""
+
+    def __init__(
+        self,
+        classifier: torch.nn.Module,
+        epsilon: Union[int, float],
+        clamp_values: Optional[tuple[Union[int, float], Union[int, float]]] = (0, 1)
+    ) -> None:
+        """
+        Args:
+            classifier: A trained classifier.
+            epsilon: Attack step size.
+            clamp_values: Valid input range.
+        """
+        self.classifier = classifier
         self.epsilon = epsilon
         self.clamp_min, self.clamp_max = clamp_values
 
-    def generate(self, inputs, labels):
+    def generate(self, inputs: Tensor, labels: Tensor) -> tuple[Tensor, Tensor]:
+        """
+        Args:
+            inputs: A tensor with shape [N, C, H, W].
+            labels: A tensor with shape [N].
+        """
         inputs.requires_grad = True
 
         # Zero gradient buffers
-        self.model.zero_grad()
+        self.classifier.zero_grad()
 
         # Forward pass
-        outputs = self.model(inputs)
+        outputs = self.classifier(inputs)
         loss = F.cross_entropy(outputs, labels)
 
         # Backward pass
@@ -28,7 +56,6 @@ class FastGradient:
         adversarial_inputs = inputs + self.epsilon * sign_inputs_grad
         adversarial_inputs = adversarial_inputs.clamp(self.clamp_min, self.clamp_max)
 
-        # Diff
-        predicted_labels = self.model(adversarial_inputs).argmax(dim=1)
-        
-        return adversarial_inputs, predicted_labels
+        adversarial_predicted_labels = self.classifier(adversarial_inputs).argmax(dim=1)
+
+        return adversarial_inputs, adversarial_predicted_labels
--- robustness/attacks/projected_gradient_descent.py
+++ robustness/attacks/projected_gradient_descent.py
@@ -1,19 +1,50 @@
+"""Projected Gradient Descent (PGD) adversarial attack.
+
+Projected Gradient Descent from 'Towards Deep Learning Models Resistant to Adversarial Attacks'
+[https://arxiv.org/abs/1706.06083]
+
+    Typical usage example:
+
+    attack = ProjectedGradientDescent(classifier, epsilon=0.03, alpha=0.2, steps=7)
+    adversarial_inputs, predicted_labels = attack.generate(inputs, labels)
+"""
+from typing import Optional, Union
+
 import torch
+from torch import Tensor
 import torch.nn.functional as F
 
 class ProjectedGradientDescent:
-    """
-    Projected Gradient Descent (PGD) from 'Towards Deep Learning Models Resistant to Adversarial Attacks'
-    [https://arxiv.org/abs/1706.06083]
-    """
-    def __init__(self, model, epsilon=0.03, alpha=0.2, steps=7, clamp_values=(0, 1)):
-        self.model = model
+    """Projected Gradient Descent adversarial attack with L_inf distance measure."""
+
+    def __init__(
+        self,
+        classifier: torch.nn.Module,
+        epsilon: Union[int, float],
+        alpha: Union[int, float],
+        steps: int,
+        clamp_values: Optional[tuple[int, int]] = (0, 1)
+        ) -> None:
+        """
+        Args:
+            classifier: A trained classifier.
+            epsilon: Maximum pertubation distance.
+            alpha: Attack step size.
+            steps: Number of steps.
+            clamp_values: Valid input range.
+        """
+        self.classifier = classifier
         self.epsilon = epsilon
         self.alpha = alpha
         self.steps = steps
         self.clamp_min, self.clamp_max = clamp_values
 
-    def generate(self, inputs, labels):
+    def generate(self, inputs: Tensor, labels: Tensor) -> tuple[Tensor, Tensor]:
+        """
+        Args:
+            inputs: A tensor with shape [N, C, H, W].
+            labels: A tensor with shape [N].
+        """
 
         adversarial_inputs = inputs.detach().clone()
         adversarial_inputs = inputs + torch.empty_like(inputs).uniform_(-self.epsilon, self.epsilon)
@@ -24,10 +55,10 @@ class ProjectedGradientDescent:
             adversarial_inputs.requires_grad = True
 
             # Zero gradient buffers
-            self.model.zero_grad()
+            self.classifier.zero_grad()
 
             # Forward pass
-            outputs = self.model(adversarial_inputs)
+            outputs = self.classifier(adversarial_inputs)
             loss = F.cross_entropy(outputs, labels)
 
             # Backward pass
@@ -39,7 +70,6 @@ class ProjectedGradientDescent:
             adversarial_inputs = adversarial_inputs.clamp(inputs - self.epsilon, inputs + self.epsilon)
             adversarial_inputs = adversarial_inputs.clamp(self.clamp_min, self.clamp_max)
 
-            # Diff
-            predicted_labels = self.model(adversarial_inputs).argmax(dim=1)
-        
-        return adversarial_inputs, predicted_labels
+            adversarial_predicted_labels = self.classifier(adversarial_inputs).argmax(dim=1)
+
+        return adversarial_inputs, adversarial_predicted_labels
